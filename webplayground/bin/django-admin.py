38a9421d68aa12efcdf8de7b2f7bf325d6c31ceb
sherstib <spico1@udi.edu.co>
2018-11-25 07:39:41 -0500

    inicio del proyecto, basado en el tutorial de udemy.

---
+++ webplayground/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/sher/Desktop/web-playground-curso-django-2-master/webplayground/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
